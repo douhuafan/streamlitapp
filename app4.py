b4ae848c8de3dd89c4d0616fc904724590fb1a47
douhuafan <13581976632@163.com>
2024-10-09 20:06:25 +0800

    Update app4.py

--- app4.py
+++ app4.py
@@ -11,6 +11,7 @@ path = os.getcwd().replace("\\", "/")
 
 if not os.path.exists(path+'/extra_trees_classifier4.joblib'):
     merge = Merge(path+"/model", path, "extra_trees_classifier4.joblib")
+    st.info("dfgh")
 else:
     pass
 
